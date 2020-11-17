1fe37a52ad1f4eebbe2e8f2246da8af31698b947
Tracy <57853991+TracyWynter@users.noreply.github.com>
2020-11-17 16:44:18 +0800

    merge

--- src/main/AlertEngine.java
+++ src/main/AlertEngine.java
@@ -1,4 +1,3 @@
-
 package main;
 
 import java.io.File;
@@ -10,53 +9,49 @@ import java.util.Collections;
 import java.util.Scanner;   // To use Scanner class to read in data from files
 import main.stats;
 
-public class AlertEngine{
+public class AlertEngine {
     // alert engine 
     //To check consitency between "live data" and the base line statistics
-    
+
     //Method for getting the new filename
-    public String getUserInputNewStatFile()
-    {
-        
+    public String getUserInputNewStatFile() {
+
         Scanner sct = new Scanner(System.in); // Create new Scanner object
-        
+
         // Prompt user for file that contains new statistics
         //Same format as Stats.txt but different parameters
         System.out.println("Please enter the filename: ");
-        
+
         //Reading user input
         String newStatsFile = sct.nextLine();
         return newStatsFile;
     }
-    
+
     //Method for getting the number of days from user
-    public Integer getUserInputNumOfDays()
-    {
+    public Integer getUserInputNumOfDays() {
         Scanner sct = new Scanner(System.in); // Create new Scanner object
-        
+
         String numOfDays;
-        int noOfDays =0;
+        int noOfDays = 0;
         System.out.println();
-        
+
         //Prompt user to enter number of days
-	System.out.println("\nPlease enter the number of days you would like to generate: ");
-        
+        System.out.println("\nPlease enter the number of days you would like to generate: ");
+
         //Reading numOfDays input as String
         numOfDays = sct.nextLine();
         //Converting user input to integer
         noOfDays = Integer.parseInt(numOfDays);
         return noOfDays;
     }
-    
+
     //Method to read in new statistics file
-    public void readNewStatFile(ArrayList<stats> newStatList) 
-    {
-	try 
-	{
+    public void readNewStatFile(ArrayList<stats> newStatList) {
+        try {
             //Calling getUserInputNewStatFile to get the file that contains new statistics
             String newStatsFile = getUserInputNewStatFile();
             //int noOfDays = getUserInputNumOfDays();
-                        
+
             File fileObj = new File(newStatsFile); // Create new File opbject
             Scanner sct = new Scanner(fileObj); // Create new Scanner object
 
@@ -64,54 +59,45 @@ public class AlertEngine{
             int eventCount = Integer.valueOf(numberOfEvents); // Convert to Integer
 
             int n = 0; // Counter to check number or records
-            while (sct.hasNextLine()) 
-            {
-		String eventText = sct.nextLine(); // Takes in next line
+            while (sct.hasNextLine()) {
+                String eventText = sct.nextLine(); // Takes in next line
                 String[] statsLine = eventText.split(":"); // Delimit 
 
-		String eventName = statsLine[0]; // Stores Event name
-		double mean = Double.valueOf(statsLine[1]); // Stores Mean value
-		double sd = Double.valueOf(statsLine[2]); // Stores Standard Deviation
-		n++;
+                String eventName = statsLine[0]; // Stores Event name
+                double mean = Double.valueOf(statsLine[1]); // Stores Mean value
+                double sd = Double.valueOf(statsLine[2]); // Stores Standard Deviation
+                n++;
                 //Storing the data for processing at the next step
-		newStatList.add(new stats(eventName, mean, sd)); 
+                newStatList.add(new stats(eventName, mean, sd));
             }
             sct.close();
 
             if (n == eventCount) // [3] Check number of events against first digit in the file.
             {// If it matches
-		System.out.println("Successfully read in " + newStatsFile + "!");
-            } 
-            else 
-            {// If it does not match
-		System.out.println("Failed to read in " + newStatsFile + "!");
-		System.out.println("Number of records specified does not match the number of records listed!");
-		System.out.println("Program will terminate!");
-		System.exit(0);
+                System.out.println("Successfully read in " + newStatsFile + "!");
+            } else {// If it does not match
+                System.out.println("Failed to read in " + newStatsFile + "!");
+                System.out.println("Number of records specified does not match the number of records listed!");
+                System.out.println("Program will terminate!");
+                System.exit(0);
             }
-	} 
-	catch (IOException e) 
-	{
+        } catch (IOException e) {
             System.err.println(e);
             System.exit(0);
-	}
+        }
 
-	// Debug Code
-	//Printing data of newStatList	
-	for (stats s : newStatList)
-	{
+        // Debug Code
+        //Printing data of newStatList	
+        for (stats s : newStatList) {
             System.out.println(s);
-	}
-} 
+        }
+    }
 
-    public void alertEngine(ArrayList<stats> newStatList) 
-    {	
+    public void alertEngine(ArrayList<stats> newStatList) {
         // Read and store data
         readNewStatFile(newStatList);
 
         // Run activity engine and produce data for the number of days specified
-
         // Run analysis engine to produce daily totals
-
     }
 }
--- src/main/IDS.java
+++ src/main/IDS.java
@@ -4,10 +4,9 @@ package main;
 // Driver Class
 import java.util.ArrayList;
 
-public class IDS 
-{
-    public static void main(String[] args) 
-    {
+public class IDS {
+
+    public static void main(String[] args) {
         // Class Object
         Input ipt = new Input();
         SimulateEngine sim = new SimulateEngine();
@@ -17,30 +16,32 @@ public class IDS
         // Custom File Objects
         ArrayList<events> eventList = new ArrayList<>();
         ArrayList<stats> statList = new ArrayList<>();
-		ArrayList<stats> newStatList = new ArrayList<>();
-        
+        ArrayList<stats> newStatList = new ArrayList<>();
+
         System.out.println("Verifying Integerity of Event.txt and Stats.txt...");
 
         // check for the right number of args length
-        if (args.length == 4) 
-        {
+        if (args.length == 4) {
             // Checks if parameters are correct
             boolean valid = ipt.validateInput(args);
 
             int days = Integer.valueOf(args[3]); // get days 
 
-            if (valid == true) 
-            {
+            if (valid == true) {
                 System.out.println("Valid Events.txt and Stats.txt");
                 System.out.println("\nProceeding to read in files...");
                 // args[1] -> Events.txt || args[2] -> Stats.txt
                 ipt.startInput(args[1], args[2], eventList, statList); // initial input
- 
-                sim.simulateEngine(days, statList, eventList); // simulate engine
-                //analysis.analysisEngine(); 
-				alert.alertEngine(newStatList); //alertEngine
 
-            } else {System.out.println("Invalid arguments");}   
-        } else {System.out.println("Invalid arguments or length");}
+                sim.simulateEngine(days, statList, eventList); // simulate engine
+                //analysis.analysisEngine(); // analyse baseline data
+                alert.alertEngine(newStatList); //alertEngine
+
+            } else {
+                System.out.println("Invalid arguments");
+            }
+        } else {
+            System.out.println("Invalid arguments or length");
+        }
     }
 }
